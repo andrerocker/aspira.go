6ba6333c377af8807c25f7fffbd60d92fe036a6e
andrerocker <andre.souza@gmail.com>
2014-06-17 13:39:33 -0300

    estava com bug ao perguntar para o bot, agora a parada vai ficar louca!

--- aspira/aspira.go
+++ aspira/aspira.go
@@ -1,20 +1,14 @@
 package aspira
 
 import (
-    "fmt"
     "./util"
     "strings"
-    "net/url"
     "net/http"
     "io/ioutil"
 )
 
 func Ask(question string) string {
-    serviceUrl := "http://www.ed.conpet.gov.br/mod_perl/bot_gateway.cgi?"
-    serviceParams := "server=0.0.0.0:8085&charset_post=utf-8&pure=1&js=0&tst=1&msg="+question
-    endpoint := serviceUrl+serviceParams
-
-    response, err := http.Get(endpoint)
+    response, err := http.Get(util.AskEndpoint(question))
     defer response.Body.Close()
 
     if err != nil {
@@ -31,15 +25,7 @@ func Ask(question string) string {
 }
 
 func Say(message string) {
-    endpoint := "https://.slack.com/services/hooks/incoming-webhook?token="
-    body := fmt.Sprintf(`{
-        "channel": "#general",
-        "username": "aspira",
-        "text": "%s",
-        "icon_url": "http://sinistersaladmusikal.files.wordpress.com/2011/01/100_9002.jpg?w=50"
-    }`, util.RemoveHtml(message))
-
-    response, _  := http.PostForm(endpoint, url.Values{"payload": {body}})
+    endpoint := util.ResponseEndpoint()
+    response, _  := http.PostForm(endpoint, util.ResponsePostBody(message))
     defer response.Body.Close()
 }
-
--- aspira/util/util.go
+++ aspira/util/util.go
@@ -1,8 +1,10 @@
 package util
 
 import (
+    "fmt"
     "regexp"
     "strings"
+    "net/url"
 )
 
 func Clean(message string) string {
@@ -12,4 +14,35 @@ func Clean(message string) string {
 func RemoveHtml(message string) string {
     pattern := regexp.MustCompile("<[^<>]*>")
     return pattern.ReplaceAllString(message, "")
+}
+
+func AskEndpoint(question string) string {
+    params := url.Values{}
+    params.Add("server", "0.0.0.0:8085")
+    params.Add("charset_post", "utf-8")
+    params.Add("pure", "1")
+    params.Add("js", "0")
+    params.Add("tst", "1")
+    params.Add("msg", question)
+
+    endpoint, _ := url.Parse("http://www.ed.conpet.gov.br/mod_perl/bot_gateway.cgi?")
+    endpoint.RawQuery = params.Encode()
+
+    return endpoint.String()
+}
+
+func ResponseEndpoint() string {
+    endpoint := "https://.slack.com/services/hooks/incoming-webhook?token="
+    return endpoint
+}
+
+func ResponsePostBody(message string) url.Values {
+    body := fmt.Sprintf(`{
+        "channel": "#hacker",
+        "username": "aspira",
+        "text": "%s",
+        "icon_url": "http://sinistersaladmusikal.files.wordpress.com/2011/01/100_9002.jpg?w=50"
+    }`, RemoveHtml(message))
+
+    return url.Values { "payload": {body} }
 }
